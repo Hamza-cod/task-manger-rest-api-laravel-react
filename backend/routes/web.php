8df5173c3b6ccef593c69294db4d12650a735a2c
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-09 14:57:28 +0100

    handel problems ya hahahaha

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,10 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-return function () {
-
+Route::get('/',function(){
     return view('welcome');
-
-};
-
+});
 
