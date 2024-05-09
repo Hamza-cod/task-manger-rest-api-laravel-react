eb92fe94c1044e192b42fb0a4c474023a228236c
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-09 15:26:58 +0100

    handel route problems

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/',function(){
+Route::get('/app',function(){
     return response()->json([
         "app"=>"task manager",
     ]);
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-// Route::get('/',function(){
-//     return view('welcome');
-// });
+Route::get('/',function(){
+    return redirect('/api/app');
+});
+
 
