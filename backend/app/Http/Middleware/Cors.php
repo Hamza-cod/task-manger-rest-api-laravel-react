c0acfffd8a418825abee26f114544d029b07191b
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-11 19:33:36 +0100

    handel cors problem and add front end url  to env

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Origin', env('FRONTEND_URL', 'http://localhost:3000'))
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', "Content-Type, Accept, Authorization, X-Requested-With, Application");
     }
