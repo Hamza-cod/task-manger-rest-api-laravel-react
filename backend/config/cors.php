2b53a2984623607af0ecae5c725beeb85a17ce97
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-11 21:38:31 +0100

    test test

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL','http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
