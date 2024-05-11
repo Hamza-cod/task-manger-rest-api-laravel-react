a11c13a819ff170a7a7d3935ba310529af7aaf5b
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-11 22:38:11 +0100

    testing

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -5,17 +5,15 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mime\Header;
 
 class Cors
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (Header)  $next
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    
-    public function handle(Request $request, Closure $next) : Header
+    public function handle(Request $request, Closure $next) : Response
     {
         return $next($request)->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', '*')
