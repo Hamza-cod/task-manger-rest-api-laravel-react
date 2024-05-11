29ad9a1133d6315f8070726a0b20f7719c2bee0f
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-11 22:32:50 +0100

    trtr

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -15,8 +15,7 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
+        return $next($request)->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', '*')
       ->header('Access-Control-Allow-Headers',' Origin, Content-Type, Accept, Authorization, X-Request-With')
       ->header('Access-Control-Allow-Credentials',' true');
