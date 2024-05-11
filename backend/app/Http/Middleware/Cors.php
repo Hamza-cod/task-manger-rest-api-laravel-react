973ba2cc49c22becddfbd2dc9a7f958597440538
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-11 22:35:37 +0100

    handele headers

--- backend/app/Http/Middleware/Cors.php
+++ backend/app/Http/Middleware/Cors.php
@@ -5,15 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Header\Headers;
 
 class Cors
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Closure(\Illuminate\Http\Request): (Headers)  $next
      */
-    public function handle(Request $request, Closure $next)
+    
+    public function handle(Request $request, Closure $next) : Headers
     {
         return $next($request)->header('Access-Control-Allow-Origin', '*')
       ->header('Access-Control-Allow-Methods', '*')
