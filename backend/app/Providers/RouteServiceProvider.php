3c84b412ceb34244bc986b8ec95aaf3df783524a
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-05-11 16:38:53 +0100

    fix err routes

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -32,9 +32,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
-            Route::middleware('cors')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+           
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
